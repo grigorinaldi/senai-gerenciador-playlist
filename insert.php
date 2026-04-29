6e9f2b5a60efeae95193d53acfe29894eeb5a28f
Giovanni <grigorinaldi@gmail.com>
2026-04-29 20:52:51 -0300

    feat: implementa inserção de músicas no banco via insert.php

--- insert.php
+++ insert.php
@@ -1,2 +1,13 @@
 <?php
+
 require_once 'crud.php';
+
+$novaMusica = [
+    'titulo' => 'Don´t Stop Me Now',
+    'artista' => 'Queen',
+    'tipo' => 'Rock',
+    'duracao' => '00:03:29',
+];
+
+$idMusicaNova = create($pdo, 'musicas', $novaMusica);
+echo 'novo livro inserido com ID: '.$idMusicaNova;
