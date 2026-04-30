934fab0d2f70db58dcc4c4e42d70478ba45142a6
Giovanni <grigorinaldi@gmail.com>
2026-04-29 21:00:47 -0300

    test: valida atualização de músicas no banco com dados fixos

--- update.php
+++ update.php
@@ -1,2 +1,19 @@
 <?php
 require_once 'crud.php';
+
+$idMusica = 3;
+
+$dadosAtualizados = [
+    'titulo' => 'The Less I Know Better',
+    'artista' => 'Tame Impala',
+    'tipo' => 'Rock psicodélico',
+    'duracao' => '00:07:38',
+];
+
+$linhasAfetadas = update($pdo, 'musicas', $dadosAtualizados, "id = $idMusica");
+
+if ($linhasAfetadas > 0){
+    echo "Música atualizada com sucesso!" . $linhasAfetadas;
+} else {
+    echo "Não foi possível atualizar a música!";
+}
